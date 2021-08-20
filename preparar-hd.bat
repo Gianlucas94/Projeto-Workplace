a0228c42fd991423faa128a5b26ddb596be3d776
Gianlucas Almeida <giandoscp@hotmail.com>
2021-08-20 14:34:06 -0300

    Preparar HD

--- preparar-hd.bat
+++ preparar-hd.bat
@@ -1,1 +1,30 @@
-test
+@pushd "%~dp0"
+@title Projeto WorkPlace - Parte 0 v.1.6
+@echo ------------------------------------------------------------------------------
+@echo  Projeto WorkPlace - Parte 0 v.1.6
+@echo ------------------------------------------------------------------------------
+@color b
+@echo .########.####.##.....##.####.########
+@echo ....##.....##..##.....##..##.....##...
+@echo ....##.....##..##.....##..##.....##...
+@echo ....##.....##..##.....##..##.....##...
+@echo ....##.....##...##...##...##.....##...
+@echo ....##.....##....##.##....##.....##...
+@echo ....##....####....###....####....##...
+@echo. 
+@cd \
+@echo ## ATENCAO - EXECUTAR LOGADO NO DOMINIO PRAXAIR ##
+@echo ## ATENCAO - BAT PARA PREPARAR HD EXTERNO PARA USO DAS BATS DO PROJETO WORKPLACE ##
+@echo.
+@set /p letra_HD= Letra do HD Externo:
+@echo ------------------------------------
+@echo  Backup - Copiando instaladores
+@echo ------------------------------------
+
+
+:: Verifica se há scripts e instaladores antigos
+@del "%letra_HD%:\scripts"
+@del "%letra_HD%:\Programas_Linde"
+
+:: Copiando instaladores para o HD Externo
+@robocopy \\brariofs01\transfer\Gian "%letra_HD%:\" /r:1 /w:1 /e /eta
