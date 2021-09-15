b9e77e14f9983dafac16bdc6cf6014a8e71f5525
Gianlucas Almeida <giandoscp@hotmail.com>
2021-09-15 15:39:02 -0300

    fix

--- 1 - backup.bat
+++ 1 - backup.bat
@@ -86,25 +86,19 @@
 @mkdir %nome%
 @cd %letra%:\backup\%nome%\
 @mkdir logs
-@echo Backup perfil do usuário > %letra%:\backup\%nome%\logs\log_backup.txt
-@echo. >> %letra%:\backup\%nome%\logs\log_backup.txt
+
 @choice.exe /c sn /m "Deseja fazer backup do OneDrive?"
-    @if errorlevel 1 robocopy C:\users\%user% "%letra%:\backup\%nome%\user files" /tee /r:1 /w:1 /e /eta /xd C:\users\%user%\AppData "C:\users\%user%\Local Settings" "Application Data" /log+:%letra%:\backup\%nome%\logs\log_backup.txt
-    @if errorlevel 2 robocopy C:\users\%user% "%letra%:\backup\%nome%\user files" /tee /r:1 /w:1 /e /eta /xd C:\users\%user%\AppData "C:\users\%user%\Local Settings" onedrive "Application Data" /log+:%letra%:\backup\%nome%\logs\log_backup.txt
-@echo. >> %letra%:\backup\%nome%\logs\log_backup.txt
-@echo Backup C:\ >> %letra%:\backup\%nome%\logs\log_backup.txt
-    @robocopy C:\ %letra%:\backup\%nome%\c_raiz /tee /e /eta /r:1 /w:1 /XD "Program Files (x86)" Windows "Program Files" "Out-of-Box Drivers" Intel users Notes.old Notesold bginfo Perflogs ProgramData "Documents and Settings" $Recycle.Bin dell Config.msi Drivers /log+:%letra%:\backup\%nome%\logs\log_backup.txt
-@echo. >> %letra%:\backup\%nome%\logs\log_backup.txt
-@echo Backup favoritos Chrome/Edge >> %letra%:\backup\%nome%\logs\log_backup.txt
-    @echo d|xcopy "C:\users\%user%\AppData\Local\Google\Chrome\User Data\Default\Bookmarks" %letra%:\backup\%nome%\Favoritos\Chrome /i >> %letra%:\backup\%nome%\logs\log_backup.txt
-    @echo d|xcopy "C:\users\%user%\AppData\Local\Microsoft\Edge\User Data\Default\Bookmarks" %letra%:\backup\%nome%\Favoritos\Edge /i >> %letra%:\backup\%nome%\logs\log_backup.txt
+@if errorlevel 1 robocopy C:\users\%user% "%letra%:\backup\%nome%\user files" /tee /r:1 /w:1 /e /eta /xd C:\users\%user%\AppData "C:\users\%user%\Local Settings" "Application Data" /log+:%letra%:\backup\%nome%\logs\log_backup.txt
+@if errorlevel 2 robocopy C:\users\%user% "%letra%:\backup\%nome%\user files" /tee /r:1 /w:1 /e /eta /xd C:\users\%user%\AppData "C:\users\%user%\Local Settings" onedrive "Application Data" /log+:%letra%:\backup\%nome%\logs\log_backup.txt
+@robocopy C:\ %letra%:\backup\%nome%\c_raiz /tee /e /eta /r:1 /w:1 /XD "Program Files (x86)" Windows "Program Files" "Out-of-Box Drivers" Intel users Notes.old Notesold bginfo Perflogs ProgramData "Documents and Settings" $Recycle.Bin dell Config.msi Drivers
+@echo d|xcopy "C:\users\%user%\AppData\Local\Google\Chrome\User Data\Default\Bookmarks" %letra%:\backup\%nome%\Favoritos\Chrome /i
+@echo d|xcopy "C:\users\%user%\AppData\Local\Microsoft\Edge\User Data\Default\Bookmarks" %letra%:\backup\%nome%\Favoritos\Edge /i
 @attrib -h -s %letra%:\backup\%nome%\c_raiz
 
 :: Criando pastas e exportando registros
 @%letra%:
 @cd %letra%:\backup\%nome%
 @mkdir registro
-@mkdir logs
 @cd %letra%:\scripts
 
 @chgcolor 02
@@ -114,37 +108,14 @@
 @chgcolor 07
 @cd %letra%:\backup\%nome%\registro
 @echo Registros exportados >> %letra%:\backup\%nome%\logs\log_backup.txt
-@reg export "HKEY_CURRENT_USER\Network" mapeamento.reg /y >> %letra%:\backup\%nome%\logs\log_backup.txt
-@reg export "HKEY_CURRENT_USER\Printers" impressoras.reg /y >> %letra%:\backup\%nome%\logs\log_backup.txt
+@reg export "HKEY_CURRENT_USER\Network" mapeamento.reg /y
+@reg export "HKEY_CURRENT_USER\Printers" impressoras.reg /y
 
 :: Exportando lista de programas instalados
 @echo Exportando lista de softwares instalados.
 wmic /OUTPUT:%letra%:\backup\%nome%\logs\softwares_instalados.txt product get name, version
 @cd \
 
-:: Exportando Logs
-@echo. >> %letra%:\backup\%nome%\logs\log_backup.txt
-hostname >> %letra%:\backup\%nome%\logs\log_backup.txt
-@echo -------------------------------------------- >> %letra%:\backup\%nome%\logs\log_backup.txt
-@echo                Tamanho das pastas            >> %letra%:\backup\%nome%\logs\log_backup.txt
-@echo -------------------------------------------- >> %letra%:\backup\%nome%\logs\log_backup.txt
-@echo. >> %letra%:\backup\%nome%\logs\log_backup.txt
-@cd c:\users\%user%\ >> %letra%:\backup\%nome%\logs\log_backup.txt
-    cd >> %letra%:\backup\%nome%\logs\log_backup.txt
-%letra%:\Programas_Linde\du.exe -nobanner c:\users\%user%\ >> %letra%:\backup\%nome%\logs\log_backup.txt
-@cd c:\Notes >> %letra%:\backup\%nome%\logs\log_backup.txt
-    cd >> %letra%:\backup\%nome%\logs\log_backup.txt
-%letra%:\Programas_Linde\du.exe -nobanner c:\users\notes\ >> %letra%:\backup\%nome%\logs\log_backup.txt
-
-hostname >> %letra%:\backup\%nome%\logs\log_backup.txt
-@cd >> "%letra%:\backup\%nome%\user files"
-    @cd >> %letra%:\backup\%nome%\logs\log_backup.txt
-@%letra%:\Programas_Linde\du.exe -nobanner "%letra%:\backup\%nome%\user files" >> %letra%:\backup\%nome%\logs\log_backup.txt
-@cd %letra%:\backup\%nome%\c_raiz\Notes
-    @cd >> %letra%:\backup\%nome%\logs\log_backup.txt
-%letra%:\Programas_Linde\du.exe -nobanner %letra%:\backup\%nome%\c_raiz\Notes >> %letra%:\backup\%nome%\logs\log_backup.txt
-
-
 @echo Finalizado.
 @pause
-@popd
+
